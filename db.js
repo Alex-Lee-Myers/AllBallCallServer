2194b424a5adbb208a37c980112dc06b68b19777
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2022-02-12 10:33:19 -0500

    heroku deploy try 2.

--- db.js
+++ db.js
@@ -5,14 +5,14 @@ const Sequelize = require('sequelize');
 //         dialect: 'postgres',
 // })
 //! LocalHost Deployment
-const db = new Sequelize(`postgres://postgres:${process.env.DbPass}@localhost:5432/AllBallCall`, {
-    dialect: 'postgres',
-    });
+// const db = new Sequelize(`postgres://postgres:${process.env.DbPass}@localhost:5432/AllBallCall`, {
+//     dialect: 'postgres',
+//     });
 
 //!Heroku Deployment
-// const db = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres',
-// });
+const db = new Sequelize(`${process.env.DATABASE_URL}`, {
+    dialect: 'postgres',
+});
 
 
 module.exports = db;
