73b324abec5d0974388306f9c815b9019c9f7a7b
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2022-02-14 00:18:13 -0500

    message

--- db.js
+++ db.js
@@ -5,22 +5,22 @@ const Sequelize = require("sequelize");
 //         dialect: 'postgres',
 // })
 //! LocalHost Deployment
-const db = new Sequelize(
-	`postgres://postgres:${process.env.DbPass}@localhost:5432/AllBallCall`,
-	{
-		dialect: "postgres",
-	}
-);
+// const db = new Sequelize(
+// 	`postgres://postgres:${process.env.DbPass}@localhost:5432/AllBallCall`,
+// 	{
+// 		dialect: "postgres",
+// 	}
+// );
 
 //!Heroku Deployment
-// const db = new Sequelize(`${process.env.DATABASE_URL}`, {
-// 	dialect: "postgres",
-// 	dialectOptions: {
-// 		ssl: {
-// 			require: true,
-// 			rejectUnauthorized: false,
-// 		},
-// 	},
-// });
+const db = new Sequelize(`${process.env.DATABASE_URL}`, {
+	dialect: "postgres",
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
+	},
+});
 
 module.exports = db;
